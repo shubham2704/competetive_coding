d44719c91861651afa4e18b76465e73ad12f86cf
shubham goswami <cool.shhubham2704@gmail.com>
2020-06-10 14:00:52 +0530

    solved chef and icecream

--- chefAndIcecream.py
+++ chefAndIcecream.py
@@ -1,27 +1,46 @@
-t = int(input())
-
-for _ in range(t):
+for _ in range(int(input())):
     n = int(input())
     l = list(map(int,input().split()))
     
-    ic = 5
-    c = []
-    
+    c = l.count(15)
+    counter = 0
     if l[0]>5:
         print("NO")
-    else:
+
+    elif c>=1:
+        b = 0
         for i in l:
-            if i-ic==0:
-                c.append(5)
-            elif i-ic==5:
-                c.append(5)
-                c.pop()
-            else:
-                c.append(5)
-                c.pop()
-                c.pop()
-        
-        if len(c)>0:
+            ret = i-5
+            if ret>0 and b<ret:
+                print('NO')
+                break
+            if ret>0 and b>=ret:
+                b+=5
+                b-=ret
+            if ret == 0 :
+                b+=5 
+            counter+=1
+            
+        if counter==len(l):
             print("YES")
         else:
-            print("NO")
+            pass
+
+    else:
+        fi,te = 0,0
+        for i in l:
+            if i==5:
+                fi+=1
+            else:
+                if fi>=1:
+                    fi-=1
+                    te+=1
+                else:
+                    print("NO")
+                    break
+            counter+=1
+
+        if counter==len(l):
+            print("YES")
+        else:
+            pass
