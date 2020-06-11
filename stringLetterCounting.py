da57a7be52a8f2269f70ef8357ff657bae72b658
shubham2704 <cool.shubham2704@gmail.com>
2020-06-11 11:26:31 +0530

    added submission link

--- stringLetterCounting.py
+++ stringLetterCounting.py
@@ -3,3 +3,5 @@ from collections import Counter
 def string_letter_count(s):
     cnt = Counter(c for c in s.lower() if c.isalpha())
     return ''.join(str(n)+c for c,n in sorted(cnt.items()))
+
+submissionLink = 'https://docs.google.com/forms/d/e/1FAIpQLSdvCKLUV1fuapmRnnUQ8EH0I42A6E-yUKtU4CK2A4CIBjLvlg/viewform?usp=pp_url&entry.1449005772=Your+Name&entry.1685270148=Your+Instagram+Username'
