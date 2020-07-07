44e70429e02d1ea80e63b4162f8606f6b161568d
shubham2704 <cool.shubham2704@gmail.com>
2020-07-07 20:21:09 +0530

    card game from july challenge 2020

---
+++ cardGame.py
@@ -0,0 +1,44 @@
+for _ in range(int(input())):
+    n = int(input())
+    a,b = [],[]
+    chef,morty = 0,0
+
+    for _ in range(n):
+        c,m = map(int,input().split())
+        def getSum(num):
+            s = 0
+            while num>0:
+                s += int(num%10)
+                num = int(num/10)
+            return s
+        a.append(getSum(c))
+        b.append(getSum(m))
+
+
+    for x,y in zip(a,b):
+        if x>y:
+            chef+=1
+        elif x==y:
+            chef+=1
+            morty+=1
+        else:
+            morty+=1
+
+    if chef>morty:
+        print(0 , chef)
+    elif morty>chef:
+        print(1 , morty)
+    else:
+        print(2 , chef) 
+
+
+"""
+2
+3
+10 4
+8 12
+7 6
+2
+5 10
+3 4
+"""
