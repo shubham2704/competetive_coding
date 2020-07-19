cb1ccb07346d8a087ace6d6ef564c600ca0c85b5
shubham2704 <cool.shubham2704@gmail.com>
2020-07-19 16:08:25 +0530

    19/07/2020 chech and cakes from codechef

---
+++ chefAndCakes.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())):
+    
+    def permute(nums):
+        result = [[]]
+        for n in nums:
+            new = []
+            for p in result:
+                for i in range(len(p)+1):
+                    new.append(p[:i] + [n] + p[i:])
+                    result = new
+        return result
+
+    n = int(input())
+    my_nums = list(map(int,input().split()))
+    l = [str(x) for x in my_nums]
+    res = permute(l)
+    print(sum(int("".join(x)) for x in res))
