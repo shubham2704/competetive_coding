9c703365c7abc1d72b81467574f00fddc35e2637
shubham2704 <cool.shubham2704@gmail.com>
2020-07-28 00:00:40 +0530

    27/07/2020 pig lating added one liner solution

--- simplePigLatin.py
+++ simplePigLatin.py
@@ -11,7 +11,6 @@ def pig_it(text):
             b = i[1:] + i[:1] + "ay"
             m.append(b)
     return " ".join(m)
-
 print(pig_it('Hello world !'))
 
 # Pro Version
@@ -19,5 +18,11 @@ def pig_it1(text):
     l = list(map(str,text.split(" ")))
     pun = ['!','?',',']
     return " ".join(x if x in pun else x[1:]+x[:1]+'ay' for x in l)
-
 print(pig_it1('Pig latin is cool'))
+
+# Pro version 2
+
+def pig_it2(text):
+    return " ".join(x[1:]+x[:1]+'ay' if x.isalpha() else x for x in text.split(" "))
+
+print(pig_it2('Pig latin is cool'))
