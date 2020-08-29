b425f29048bae1c9f2f427e1d96556a1392402af
shubham2704 <cool.shubham2704@gmail.com>
2020-08-29 20:53:44 +0530

    1 removed

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,31 +1,3 @@
-"""
-Find the next perfect square!
-
-You might know some pretty large perfect squares. But what about the NEXT one?
-
-Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
-
-If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is positive.
-
-Examples:
-
-findNextSquare(121) --> returns 144
-findNextSquare(625) --> returns 676
-findNextSquare(114) --> returns -1 since 114 is not a perfect
-
-
-Solution 
-
-def find_next_square(sq):
-    # Return the next square if sq is a square, -1 otherwise
-    sqr = sq**0.5
-    n = sqr+1
-    if int(sqr+0.5)**2 == sq :
-        return n**2
-    else:
-        return -1
-"""
-
 
 """
 Array Difference
