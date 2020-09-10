f773aa62bb380bb84a3f4a2f940248e90460d94e
shubham2704 <cool.shubham2704@gmail.com>
2020-09-11 00:00:00 +0530

    5 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,28 +1,4 @@
 """
-Sorted Odd
-
-
-You have an array of numbers.
-Your task is to sort ascending odd numbers but even numbers must be on their places.
-
-Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
-
-Example
-
-sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
-
-
-Solution
-
-def sort_array(source_array):
-    # Return a sorted array.
-    o = iter(sorted(e for e in source_array if e%2))    
-    return [next(o) if e%2 else e for e in source_array]
-
-"""
-
-
-"""
 
 The Amount of Points
 
