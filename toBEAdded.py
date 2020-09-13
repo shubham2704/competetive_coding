1af6f637acfbcad2df1235aaeba2ad16a625bded
shubham2704 <cool.shubham2704@gmail.com>
2020-09-13 18:49:20 +0530

    7 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,21 +1,4 @@
 """
-Camel case Method
-
-Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
-
-For instance:
-
-camelcase("hello case") = > HelloCase
-camelcase("camel case word") => CamelCaseWord
-
-Solution
-
-def camel_case(string):
-    return "".join(x for x in string.title() if not x.isspace())
-
-"""
-
-"""
 
 Abbreviate a Two Word Name
 
