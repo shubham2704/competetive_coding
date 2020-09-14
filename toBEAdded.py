1355ace47ecf615080ebc49c149948938a6337e7
shubham2704 <cool.shubham2704@gmail.com>
2020-09-14 23:11:55 +0530

    8 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,27 +1,5 @@
 """
 
-Abbreviate a Two Word Name
-
-Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-
-The output should be two capital letters with a dot separating them.
-
-It should look like this:
-
-Sam Harris => S.H
-
-Patrick Feeney => P.F
-
-
-Solution
-
-def abbrevName(name):
-    return ".".join(x[0].upper() for x in name.split(" ") )
-"""
-
-
-"""
-
 Array Plus Array
 
 I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
