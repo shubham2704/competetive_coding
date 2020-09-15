b38ca32d020ccad0283e701cdb57f8fe8d151184
shubham2704 <cool.shubham2704@gmail.com>
2020-09-15 20:47:29 +0530

    8 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,20 +1,4 @@
 """
-
-Array Plus Array
-
-I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
-
-P.S. Each array includes only integer numbers. Output is a number too.
-
-
-Solution
-
-def array_plus_array(arr1,arr2):
-    return sum(arr1)+sum(arr2)
-"""
-
-
-"""
 Beginner Series #1 School Paperwork
 
 Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
