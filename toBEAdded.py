088471bc9e8d7b222963425a33e69b5fdfbedbd2
shubham2704 <cool.shubham2704@gmail.com>
2020-09-17 19:03:22 +0530

    9 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -48,7 +48,6 @@ def array_madness(a,b):
 All star coding challenge
 
 This Kata is intended as a small challenge for my students
-
 All Star Code Challenge #18
 
 Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
