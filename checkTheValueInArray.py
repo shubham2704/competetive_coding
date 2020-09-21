4a2d2240054d07303009dd6305b3a62ca5f42ae2
shubham2704 <cool.shubham2704@gmail.com>
2020-09-21 22:27:53 +0530

    element in array from codewars

---
+++ checkTheValueInArray.py
@@ -0,0 +1,3 @@
+def check(seq, elem):
+    return True if elem in seq else False
+print(check([1,2,3],4))
--- toBEAdded.py
+++ toBEAdded.py
@@ -3,7 +3,7 @@
 You only need one - Beginner
 
 
-You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+You will be given an array A and a value X. All you need to do is check whether the provided array contains the value.
 Array can contain numbers or strings. X can be either.
 Return true if the array contains the value, false if not.
 
