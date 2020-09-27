9701705afef45b0475e06ec06be91d893c8ff33f
shubham2704 <cool.shubham2704@gmail.com>
2020-09-27 23:22:14 +0530

    12 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,21 +1,4 @@
 """
-Speed code Array Madness
-
-
-Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
-
-E.g.
-
-array_madness([4, 5, 6], [1, 2, 3]) => True #because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
-
-
-Solution
-
-def array_madness(a,b):
-    return sum(x**2 for x in a)> sum(y**3 for y in b)
-"""
-
-"""
 All star coding challenge
 
 This Kata is intended as a small challenge for my students
