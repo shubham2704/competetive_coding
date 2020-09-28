859f50a4542bdd65802532ad653469074cb82ee0
shubham2704 <cool.shubham2704@gmail.com>
2020-09-28 23:10:58 +0530

    added new method to odd even

--- evenOdd.py
+++ evenOdd.py
@@ -1,5 +1,12 @@
+# Method for printing 'EVEN or ODD'
 def evenOdd(n):
     return 'Even' if n%2==0 else 'Odd'
 
+# Method for printing 'TRUE or FALSE'
+def evenodd1(n):
+    return True if n%2 else False
+
 print(evenOdd(5))
+print(evenodd1(5))
 print(evenOdd(6))
+print(evenodd1(6))
