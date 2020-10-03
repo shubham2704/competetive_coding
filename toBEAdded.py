2b70997ccef1331160b619587bb2a0da054fa879
shubham2704 <cool.shubham2704@gmail.com>
2020-10-03 10:01:33 +0530

    13 added

--- toBEAdded.py
+++ toBEAdded.py
@@ -1,28 +1,4 @@
 """
-Find Divisors
-
-Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), from smallest to largest. If the number is prime return the string '(integer) is prime' (null in C#) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
-
-Example:
-divisors(12); #should return [2,3,4,6]
-divisors(25); #should return [5]
-divisors(13); #should return "13 is prime"
-
-Solution
-
-def divisors(n):
-    if n == 1:
-        return "{} is prime".format(n)
-    else:
-        c = [x for x in range(2,n//2+1) if n%x==0]
-        if len(c)==0:
-            return "{} is prime".format(n)
-        else:
-            return c
-"""
-
-
-"""
 Beeramid
 
 
