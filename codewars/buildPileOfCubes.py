82887bdf2fa5fb1744ce9e53fb094d81eeafa9f4
shubham2704 <cool.shubham2704@gmail.com>
2020-12-24 23:27:20 +0530

    pile of cubes from codewars

---
+++ codewars/buildPileOfCubes.py
@@ -0,0 +1,11 @@
+def find_nb(m):
+    n = 1
+    volume = 0
+    while volume < m:
+        volume += n**3
+        if volume == m:
+            return n
+        n += 1
+    return -1
+
+print(find_nb(1071225))
