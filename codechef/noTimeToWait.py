de14e7034d33df774e8a62472bd48d18c28ded1e
shubham2704 <cool.shubham2704@gmail.com>
2021-03-11 23:33:59 +0530

    not time to wait from march challenge

--- codechef/noTimeToWait.py
+++ codechef/noTimeToWait.py
@@ -12,3 +12,19 @@ if c>0:
     print('yes')
 else:
     print('no')
+
+# Using Function/Method
+
+def sol(t,x,h):
+    t.sort()
+    t1 = t[::-1]
+    c = 0
+    for i in t1:
+        if x+i>=h:
+            c+=1
+            break
+    return 'yes' if c>0 else 'no'
+
+n,h,x = map(int,input().split())
+t = list(map(int,input().split()))
+print(sol(t,x,h))
