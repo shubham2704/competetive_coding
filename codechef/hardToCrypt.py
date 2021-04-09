c22612b454a084845f4c7fc1e0fa6d89875d23ab
shubham2704 <cool.shubham2704@gmail.com>
2021-04-09 20:25:08 +0530

    hard to crypt

--- codechef/hardToCrypt.py
+++ codechef/hardToCrypt.py
@@ -1,12 +1,4 @@
-# for _ in range(int(input())):
-#     n,m,k = map(int,input().split())
-#     matrix = []
-#     for i in range(n):
-#         l = list(map(int,input().split()))
-#         matrix.append(l)
-#         l = []
-#     print(matrix)
-
+# Basic Method
 n = int(input())
 arr = list(map(int,input().strip().split()))
 c = 0
@@ -16,5 +8,15 @@ for i in range(0,len(arr)-3):
             for l in range(k+1,len(arr)):
                 if(arr[l]-arr[k] == arr[k]-arr[j] and arr[k]-arr[j] == arr[j]-arr[i] and arr[l]-arr[k] == arr[j]-arr[i]):
                     c+=1
-
 print(c)
+
+# Advanced Method
+from itertools import combinations
+c=0
+n=int(input())
+l=list(map(int,input().split()))
+for i in list(combinations(l,4)):
+    if(i[1]-i[0]==i[2]-i[1]==i[3]-i[2] and (i[0]<=i[1]<=i[2]<=i[3] or i[0]>=i[1]>=i[2]>=i[3] )):
+        c+=1
+print(c)
+
