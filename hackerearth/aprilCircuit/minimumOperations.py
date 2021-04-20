6ca87cc68713a8e8e5871bebdb9357f73f021321
shubham2704 <cool.shubham2704@gmail.com>
2021-04-20 23:02:41 +0530

    hackerearth april circuit

--- hackerearth/aprilCircuit/minimumOperations.py
+++ hackerearth/aprilCircuit/minimumOperations.py
@@ -1,14 +1,23 @@
+# Method 1
 n = int(input())
 l = list(map(int,input().split()))
+
 cur = l[0]
 res = 0
+
 for x in range(1,n):
     if l[x] == cur:
-        print(f'l[x] = {l[x]} and cur = {cur} ')
         continue
     else:
         cur = l[x]
-        print(f'cur from else {cur}')
         res += 1
-print(res)
+print(res+1)
+
+
+
+
+
+
+
+
 
