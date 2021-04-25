8faa732c9fc09545a994ce9703338ecd4af28cd0
shubham2704 <cool.shubham2704@gmail.com>
2021-04-26 01:55:09 +0530

    C:/Users/cools/AppData/Local/Programs/Python/Python37/python.exe d:/projects/competetive_coding/practice/practice.py

--- practice/practice.py
+++ practice/practice.py
@@ -1,1 +1,8 @@
 
+from itertools import combinations
+
+for _ in range(int(input())):
+    l,r = map(int,input().split())
+    l = list(range(l,r+1))
+    res = sum(x[0]*x[1] for x in list(combinations(l,2)))
+    print(2*res%1000000007)
