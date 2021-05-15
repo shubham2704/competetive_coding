f47cb5e0cc0f09fd31743d51e6dd76a96b39e7a0
shubham2704 <cool.shubham2704@gmail.com>
2021-05-15 22:09:58 +0530

    may challenge:

--- codechef/solublity.py
+++ codechef/solublity.py
@@ -2,3 +2,9 @@ for _ in range(int(input())):
     x,a,b = map(int,input().split())
     res = a + (100-x)*b
     print(res*10)
+
+# Shorthand
+for _ in range(int(input())):
+    x,a,b = map(int,input().split())
+    print((a+(100-x)*b)*10)
+     
