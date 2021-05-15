2c5c4b9528f298dce093fbddb592cea470372ae4
shubham2704 <cool.shubham2704@gmail.com>
2021-05-15 22:22:16 +0530

    real life problem from codechf

---
+++ codechef/Unkonwn/UNKNOWN1.py
@@ -0,0 +1,8 @@
+n,m = map(float,input().split())
+print(n+m)
+print(n-m)
+print(n*m)
+print(round(n/m,1))
+
+# One Liner
+n,m = map(float,input().split()); print(n+m); print(n-m); print(n*m); print(round(n/m,1));
